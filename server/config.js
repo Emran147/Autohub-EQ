accdf4e7b51b70bfb5bc1bf1e563296ab7ab5803
Your username <ek29091@gmail.com>
2024-01-16 14:10:57 +0200

    Added NodeMailer Service Manager

--- server/config.js
+++ server/config.js
@@ -2,6 +2,8 @@
 class Config {
     static PORT = 3000;
     static MONGODB_URL = "mongodb://127.0.0.1/AutoHub";
+    static NODE_MAILER_EMAIL = "ek29091@gmail.com"; 
+    static NODE_MAILER_SECRET = "hekq uwjy zomx czcj"; 
 }
 
 module.exports = Config;
