5958e1f23f28dae4fd41cf4f5273d64890b49209
jaafari-R <jaafariramadan@gmail.com>
2024-01-16 15:53:45 +0200

    cleaned code

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,23 +1,3 @@
-// User.js/controllers
-// const UserModel = require("../models/user");
-
-// class MessageController {
-//     static async sendMessage(req, res) {
-//         const userId = req.userId;
-//         const { message } = req.body;
-//         try {
-//             const { email, phoneNumber } = await UserModel.getUserEmailAndPhoneNumber(userId);
-//             console.log(`Email: ${email}, Phone Number: ${phoneNumber}, Message: ${message}`);
-//             res.send("Message sent successfully");
-//         } catch (error) {
-//             console.error("Error sending message:", error.message);
-//             res.status(500).send("Internal Server Error");
-//         }
-//     }
-// }
-
-// module.exports = MessageController;
-
 const UserModel = require("../models/user");
 class UserController {
     static async postUpdateEmail(req, res) {     
--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,9 +1,10 @@
 // messages.js \routes
 const { Router } = require("express");
 const MessagesController = require("../controllers/messages")
+
 const router = Router();
 
 router.get("/:language", MessagesController.getMessagesByLanguage);
-// router.post("/send", MessageController.sendMessage)
+router.post("/send", MessagesController.sendMessage)
 
 module.exports = router;
