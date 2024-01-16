e24ff6e7c9af8871732b6ac3f56e39264ee495b8
jaafari-R <jaafariramadan@gmail.com>
2024-01-16 10:02:20 +0200

    Initialized server with db connection

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const { PORT } = require("./server/config");
+const { dbConnect } = require("./server/db/dbConnect");
 
 const app = express();
 
+app.use(express.static(__dirname + "/public"))
 
-app.listen(PORT, () => {
-    console.log("Server is listening on port " + PORT);
-})
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+async function main() {
+    await dbConnect();
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+        console.log("Server is listening on port " + PORT);
+    })
+}
+
+main();
