22a068468e71bf37a57afb131b23c19791e9e1b8
Fadi <fadi.97.kh@outlook.com>
2024-01-17 00:22:06 +0200

    Added login and register - Frontend

--- public/apiManagers/authApiManager.js
+++ public/apiManagers/authApiManager.js
@@ -2,7 +2,7 @@ class AuthApiManager {
     login(email, password) {
         return $.post("/api/auth/login", {email, password})
     }
-    register(fullName, email, phoneNumber, password) {
-        return $.post("/api/auth/register", {fullName, email, phoneNumber, password})
+    register(fullName, email, phoneNumber, password, language) {
+        return $.post("/api/auth/register", {fullName, email, phoneNumber, password, language})
     }
 }
