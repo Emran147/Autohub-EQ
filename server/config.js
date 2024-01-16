564f25efb656558cc9e952948cceb96db3cdcf8b
jaafari-R <jaafariramadan@gmail.com>
2024-01-16 18:44:24 +0200

    Added Twilio Message Manager

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
-// config.js\routes
+require("dotenv").config();
+
 class Config {
     static PORT = 3000;
     static MONGODB_URL = "mongodb://127.0.0.1/AutoHub";
@@ -6,6 +7,9 @@ class Config {
     static NODE_MAILER_SECRET = "hekq uwjy zomx czcj"; 
     static HASH_PASSWORD_SALT_ROUNDS = 10;
     static JWT_SECRET_KEY = "1235482";
+    static TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID || "";
+    static TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN || "";
+    static TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER || "";
 }
 
 module.exports = Config;
---
+++ server/services/twilioMsgs.js
@@ -0,0 +1,30 @@
+const twilio = require('twilio');
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER } = require('../config');
+
+class TwilioMessagesManager {
+    constructor() {
+        this.client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+    }
+
+    sendSMS(phoneNumber, message) {
+        this.client.messages
+        .create({
+          body: message,
+          to: phoneNumber, // Text your number
+          from: TWILIO_PHONE_NUMBER, // From a valid Twilio number
+        })
+        .then((message) => console.log("SMS Message sent with id:", message.sid));
+    }
+
+    sendWhatsapp(phoneNumber, message) {
+        this.client.messages
+        .create({
+            body: message,
+            from: "whatsapp:" + TWILIO_PHONE_NUMBER,
+            to: "whatsapp:" + phoneNumber
+        })
+        .then(message => console.log("Whatsapp Message sent with id:", message.sid))
+    }
+}
+
+module.exports = new TwilioMessagesManager();
