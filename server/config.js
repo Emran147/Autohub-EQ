0e0e7c3964a18eb045246dadd0d147bdf4115031
Emran147 <emran.qedan11@hotmail.com>
2024-01-27 13:35:08 +0200

    New branch

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 class Config {
     static PORT = 3000;
     static MONGODB_URL = "mongodb://127.0.0.1/AutoHub";
-    static NODE_MAILER_EMAIL = "ek29091@gmail.com"; 
+    static NODE_MAILER_EMAIL = ""; 
     static NODE_MAILER_SECRET = "hekq uwjy zomx czcj"; 
     static HASH_PASSWORD_SALT_ROUNDS = 10;
     static JWT_SECRET_KEY = "1235482";
